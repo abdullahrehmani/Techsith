ea4b9945ac7bfe828964291f4a46d010599db97a
Abdullah rehmani <44204503+abdullahrehmani@users.noreply.github.com>
2019-03-28 21:18:22 +0500

    react and redux

--- src/App.js
+++ src/App.js
@@ -1,69 +1,39 @@
 import React, { Component } from 'react';
+
 import './App.css';
-import PropTypes from 'prop-types'
-
-const Test=(props)=> {
-  return(
-    <div>
- <h1>{props.str}</h1>
- <h1>{props.bool ? 'bool':'no bool'}</h1>
- <h1>{props.strOrNum}</h1>
- <div>
-   {
-     props.ary.map((val)=>{
-    return(<li key={val}>{val}</li>)
-
-     })
-   }
- </div>
- <div>
-   {
-     props.aryOfObj.map((val)=>{
-    return(<li key={val.age}>{val.name}</li>)
-
-     })
-   }
- </div>
- <h1>{props.children}</h1>
-
-</div>
-   
-  )
-}
-Test.propTypes={
-  str:PropTypes.string,
-  bool:PropTypes.bool,
-  strOrNum:PropTypes.oneOfType([PropTypes.string,PropTypes.number]),
-  ary:PropTypes.arrayOf(PropTypes.number),
-  aryOfObj:PropTypes.arrayOf(PropTypes.shape(
-    {
-      name:PropTypes.string,
-      age:PropTypes.numeber
-    }
-  )),
-  children:PropTypes.string
-  
-}
-
-
-
-
+import {connect} from 'react-redux'
 class App extends Component {
+
+
+
   render() {
     return (
       <div className="App">
-      <Test 
-      bool
-      str={'Techsith'} 
-      strOrNum={10}
 
-      ary={[1,2,3]}
-      aryOfObj={[{name:'john', age:10},{name:'peter', age:15 }]}
-      >Children</Test>
- 
+  <div>Age:<span>{this.props.age}</span></div>
+  <button onClick={this.props.onAgeUP}>Age Up </button>
+  <button onClick={this.props.onAgeDown}>Age down</button>
+       
       </div>
     );
-  }
-}
+      }
+    }
+    const mapStateToProps=(state)=>{
+      return{
+        age:state.age
+      }
+    }
 
-export default App;
+    const mapDispachToProps = (dispach)=>{
+      return{
+        onAgeUP:()=> dispach({type:'AGE_UP'}),
+        onAgeDown:()=>dispach({type:'AGE_DOWN'})
+
+
+      }
+
+    }
+  
+
+
+export default connect(mapStateToProps,mapDispachToProps)(App);
